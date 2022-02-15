bc49c629a2f81fb0c46d94a94647828d1e2af47b
Jay Panchal <panchalj272@gmail.com>
2022-02-15 19:51:06 +0530

    Update config.js

---
+++ src/config.js
@@ -0,0 +1,21 @@
+/*
+ * TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+require('dotenv').config()
+
+module.exports = {
+  twitterKeys: {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  },
+  twitterConfig: {
+    queryString: process.env.QUERY_STRING,
+    username: process.env.TWITTER_USERNAME,
+  }
+}
