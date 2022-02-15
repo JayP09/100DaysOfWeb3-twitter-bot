b3f5f84acb29fd25b786c2ff3796a3449ed41de8
Jay Panchal <panchalj272@gmail.com>
2022-02-15 20:16:02 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,8 +9,8 @@ require('dotenv').config()
 
 module.exports = {
   twitterKeys: {
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET,
     access_token: process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   },
