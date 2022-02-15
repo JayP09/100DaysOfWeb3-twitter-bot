811db40553bee3909a7ee10505a2800832fe285f
Jay Panchal <panchalj272@gmail.com>
2022-02-15 22:32:48 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -12,28 +12,29 @@ const TwitterBot = new Twit(config.twitterKeys)
 var stream = TwitterBot.stream('statuses/filter', { track: ['#100DaysOfWeb3'] })
 
 const retweetAndLike = () => stream.on('tweet', function (tweet) {
-
     let retweetID = tweet.id_str
 
     // Retweet Tweet with id "retweetID"
-    TwitterBot.post('statuses/retweet/:id', { id: retweetID }, (err, res) => {
-        if (res) {
-          console.log(`==> RETWEET SUCCESS ${retweetID}`)
-        }
-        if (err) {
-          console.log(`==> ERROR in RETWEET ${err}`)
-        }
-    })
+    if(tweet.user.screen_name !== '_100DaysOfWeb3'){
+        TwitterBot.post('statuses/retweet/:id', { id: retweetID }, (err, res) => {
+            if (res) {
+            console.log(`==> RETWEET SUCCESS ${retweetID}`)
+            }
+            if (err) {
+            console.log(`==> ERROR in RETWEET ${err}`)
+            }
+        })
 
-    // Like Tweet with id "retweetID"
-    TwitterBot.post('favorites/create', { id: retweetID }, (err, res) => {
-        if (res) {
-          console.log(`==> Liked SUCCESS ${retweetID}`)
-        }
-        if (err) {
-          console.log(`==> ERROR in Like ${err}`)
-        }
-    })
+        // Like Tweet with id "retweetID"
+        TwitterBot.post('favorites/create', { id: retweetID }, (err, res) => {
+            if (res) {
+            console.log(`==> Liked SUCCESS ${retweetID}`)
+            }
+            if (err) {
+            console.log(`==> ERROR in Like ${err}`)
+            }
+        })
+    }   
 })
 
 
